81e913a395ff3f093ec7009575e0f7b818c2e190
abdullah107189 <abdullah107189@gmail.com>
2024-12-20 14:23:50 +0600

    complete sort feature in all jobs page

--- src/pages/AllJobs.jsx
+++ src/pages/AllJobs.jsx
@@ -16,9 +16,10 @@ const AllJobs = () => {
     fetchAllJobs()
   }, [searchValue, searchCategory, sortValue])
 
-  const handleSearchJobs = (e) => {
-    e.preventDefault()
-    // console.log(e.target.search.value);
+  const handleReset = () => {
+    setSearchCategory('')
+    setSearchValue('')
+    setSortValue('')
 
   }
 
@@ -40,20 +41,21 @@ const AllJobs = () => {
             </select>
           </div>
 
-          <form onSubmit={handleSearchJobs}>
+          <form>
             <div className='flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300'>
               <input
                 className='px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent'
                 type='text'
                 name='search'
+                value={searchValue}
                 onChange={(e) => setSearchValue(e.target.value)}
                 placeholder='Enter Job Title'
                 aria-label='Enter Job Title'
               />
 
-              <button className='px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none'>
+              <p className='hover:cursor-pointer px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none'>
                 Search
-              </button>
+              </p>
             </div>
           </form>
           <div>
@@ -68,7 +70,7 @@ const AllJobs = () => {
               <option value='asc'>Ascending Order</option>
             </select>
           </div>
-          <button className='btn'>Reset</button>
+          <button onClick={handleReset} className='btn'>Reset</button>
         </div>
         <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
           {
